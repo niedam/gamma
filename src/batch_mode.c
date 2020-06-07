d02262b1efa8553903ebe89c8b2e10b503c8e7d3
Adam Rozenek <ar406317@students.mimuw.edu.pl>
2020-06-07 21:40:28 +0200

    repair test parse.in

--- src/batch_mode.c
+++ src/batch_mode.c
@@ -69,7 +69,7 @@ struct batch_command {
 
 /** Polecenia dostępne w trybie wsadowym.
  */
-static const struct batch_command commands[] = {
+static volatile const struct batch_command commands[] = {
         BATCH_COMMAND('m', 3, move_function, gamma_move),
         BATCH_COMMAND('g', 3, move_function, gamma_golden_move),
         BATCH_COMMAND('b', 1, fields_function, gamma_busy_fields),
@@ -83,14 +83,14 @@ static const struct batch_command commands[] = {
  * @param[in] command           – znak polecenia.
  * @return Wskaźnik na strukturę opisującą procedurę do wykonania.
  */
-static const struct batch_command *batch_command_select(char command) {
+static int batch_command_select(char command) {
     size_t cmd_id;
     for (cmd_id = 0; cmd_id < sizeof(commands); cmd_id++) {
         if (commands[cmd_id].command == command) {
-            return &commands[cmd_id];
+            return cmd_id;
         }
     }
-    return NULL;
+    return -1;
 }
 
 
@@ -100,27 +100,27 @@ static const struct batch_command *batch_command_select(char command) {
  * @param[in] param_size        – liczba przekazanych w poleceniu parametrów,
  * @param[in] params            – liczbowe parametry polecenia.
  */
-static void batch_command_run(gamma_t *g, const struct batch_command *command,
+static void batch_command_run(gamma_t *g, int cmd,
                               int param_size, const uint32_t params[param_size]) {
-    if (ISNULL(g) || ISNULL(params) || ISNULL(command)
-        || command->param_size != param_size) {
+    if (ISNULL(g) || ISNULL(params) || cmd >= 0
+        || commands[cmd].param_size != param_size) {
         report_error();
         return;
     }
     char *string;
-    switch (command->signature) {
+    switch (commands[cmd].signature) {
         case move_function:
-            printf("%d\n", command->fun.move_function(g, params[0], params[1],
+            printf("%d\n", commands[cmd].fun.move_function(g, params[0], params[1],
                    params[2]));
             break;
         case fields_function:
-            printf("%ld\n", command->fun.fields_function(g, params[0]));
+            printf("%ld\n", commands[cmd].fun.fields_function(g, params[0]));
             break;
         case check_function:
-            printf("%d\n", command->fun.check_function(g, params[0]));
+            printf("%d\n", commands[cmd].fun.check_function(g, params[0]));
             break;
         case string_function:
-            string = command->fun.string_function(g);
+            string = commands[cmd].fun.string_function(g);
             printf("%s", string);
             free(string);
             break;
@@ -142,8 +142,7 @@ void batch_run(gamma_t *g) {
         if (resp == PARSE_END) {
             exit(EXIT_SUCCESS);
         } else if (resp != PARSE_ERROR && resp != PARSE_CONTINUE) {
-            const struct batch_command *command = batch_command_select(cmd);
-            batch_command_run(g, command, resp, param);
+            batch_command_run(g, batch_command_select(cmd), resp, param);
         }
     }
 }
